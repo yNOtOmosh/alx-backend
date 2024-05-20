5393c6a0188461fec475b8ca4f2c9332999c1fb0
yNOtOmosh <onyango.omondi.tony@gmail.com>
2024-05-20 16:33:37 +0000

    a little change task 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -15,7 +15,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
-    return start_index, end_index
+    return (start_index, end_index)
 
 
 class Server:
