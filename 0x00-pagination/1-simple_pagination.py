078ca460993a5cb6554a6b3f0afe7994d88e5278
yNOtOmosh <onyango.omondi.tony@gmail.com>
2024-05-20 14:42:55 +0000

    Simple pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""Simple helper function."""
+import csv
+import math
+from typing import List
+
+
+def index_range(page, page_size):
+    """
+    Returns a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters.
+
+    Page numbers are 1-indexed, i.e. the first page is page 1.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and page > 0
+        "Page must be a positive integer"
+        assert isinstance(page_size, int) and page_size > 0
+        "Page size must be a positive integer"
+
+        dataset = self.dataset()
+        index_range = math.ceil(len(dataset) / page_size)
+        if page > index_range:
+            return []
+
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return dataset[start_index:end_index]
