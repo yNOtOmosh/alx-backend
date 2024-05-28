bb25defc91a98ecac6f70f376c26d44ccaad4677
onyango.omondi.tony <onyango.omondi.tony@gmail.com>
2024-05-28 08:48:25 +0000

    1. Basic Babel setup and some few modifications

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,10 +7,11 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def index() -> str:
     """home page."""
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+    # app.run(host='0.0.0.0', port=5000)
