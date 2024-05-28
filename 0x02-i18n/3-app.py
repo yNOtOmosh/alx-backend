c2c10de26a96e5ee5f1ef72e7c3a61c25e167455
onyango.omondi.tony <onyango.omondi.tony@gmail.com>
2024-05-28 09:21:32 +0000

    3. Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""Parametrize templates."""
+from flask_babel import Babel
+from flask import Flask, render_template, request
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def index() -> str:
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    # app.run(host="0.0.0.0", port=5000)
