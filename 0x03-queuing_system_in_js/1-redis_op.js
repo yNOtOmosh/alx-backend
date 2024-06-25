50c1cb12103740c5e7e0220bac732f4927283c84
onyango.omondi.tony <onyango.omondi.tony@gmail.com>
2024-06-25 09:36:39 +0000

    Node Redis client and basic operations

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,30 @@
+#!/usr/bin/yarn dev
+import { createClient, print }from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+const setNewSchool = (schoolName, value) => {
+  client.set(schoolName, value, redis.print);
+};
+
+const displaySchoolValue = (schoolName) => {
+  client.get(schoolName, (err, reply) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(reply);
+    }
+  });
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
