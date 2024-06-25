e60d91e29ebcde393e7133c763b835c9fc93e923
yNOtOmosh <onyango.omondi.tony@gmail.com>
2024-06-25 09:39:51 +0000

    a change

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -1,30 +1,12 @@
 #!/usr/bin/yarn dev
-import { createClient, print }from 'redis';
+import { createClient } from 'redis';
 
-const client = redis.createClient();
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
 
 client.on('connect', () => {
   console.log('Redis client connected to the server');
 });
-
-client.on('error', (err) => {
-  console.log(`Redis client not connected to the server: ${err.message}`);
-});
-
-const setNewSchool = (schoolName, value) => {
-  client.set(schoolName, value, redis.print);
-};
-
-const displaySchoolValue = (schoolName) => {
-  client.get(schoolName, (err, reply) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(reply);
-    }
-  });
-};
-
-displaySchoolValue('Holberton');
-setNewSchool('HolbertonSanFrancisco', '100');
-displaySchoolValue('HolbertonSanFrancisco');
