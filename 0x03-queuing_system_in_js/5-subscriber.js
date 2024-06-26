fef09d0ce83b914a86600a378123f9a2d92668c2
onyango.omondi.tony <onyango.omondi.tony@gmail.com>
2024-06-26 09:29:18 +0000

    Node Redis client publisher and subscriber

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,27 @@
+#!/usr/bin/yarn dev
+import { createClient } from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+client.subscribe('holberton school channel');
+
+client.on('message', (channel, message) => {
+  if (channel === 'holberton school channel') {
+    console.log(message);
+  }
+});
+
+client.on('message', (channel, message) => {
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe();
+    client.quit();
+  }
+});
