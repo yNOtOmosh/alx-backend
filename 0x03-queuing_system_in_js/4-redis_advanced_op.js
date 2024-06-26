c43ff979da875ec701dbc7a68f19485e205059f9
onyango.omondi.tony <onyango.omondi.tony@gmail.com>
2024-06-26 09:17:56 +0000

    modification

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -1,36 +1,35 @@
 #!/usr/bin/yarn dev
-import { createClient, print } from 'redis';
+import redis from 'redis';
 
-const client = createClient();
-
-client.on('error', (err) => {
-  console.log('Redis client not connected to the server:', err.toString());
-});
-
-const updateHash = (hashName, fieldName, fieldValue) => {
-  client.HSET(hashName, fieldName, fieldValue, print);
-};
-
-const printHash = (hashName) => {
-  client.HGETALL(hashName, (_err, reply) => console.log(reply));
-};
-
-function main() {
-  const hashObj = {
-    Portland: 50,
-    Seattle: 80,
-    'New York': 20,
-    Bogota: 20,
-    Cali: 40,
-    Paris: 2,
-  };
-  for (const [field, value] of Object.entries(hashObj)) {
-    updateHash('HolbertonSchools', field, value);
-  }
-  printHash('HolbertonSchools');
-}
+const client = redis.createClient();
 
 client.on('connect', () => {
   console.log('Redis client connected to the server');
-  main();
 });
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+const setHashValues = () => {
+  client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+  client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+  client.hset('HolbertonSchools', 'New York', 20, redis.print);
+  client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+  client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+  client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+};
+
+const displayHashValues = () => {
+  client.hgetall('HolbertonSchools', (err, reply) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(reply);
+    }
+  });
+};
+
+// Set hash values and display them
+setHashValues();
+displayHashValues();
